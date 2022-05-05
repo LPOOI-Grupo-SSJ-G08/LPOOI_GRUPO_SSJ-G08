8499011bd764c8e43bbcc16bb35d69bd73ed6837
FlorNV <florencianoelivaldez@gmail.com>
2022-05-05 19:42:58 -0300

    Correcciones FrmAltaClientes

--- Vistas/FrmAltaClientes.Designer.cs
+++ Vistas/FrmAltaClientes.Designer.cs
@@ -44,6 +44,12 @@
             this.btnCancelar = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblValidNombre = new System.Windows.Forms.Label();
+            this.lblValidApellido = new System.Windows.Forms.Label();
+            this.lblValidDni = new System.Windows.Forms.Label();
+            this.lblValidDireccion = new System.Windows.Forms.Label();
+            this.lblValidCUIT = new System.Windows.Forms.Label();
+            this.lblValidNroCarnet = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,7 +58,7 @@
             this.txtNombre.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNombre.Location = new System.Drawing.Point(209, 82);
+            this.txtNombre.Location = new System.Drawing.Point(209, 74);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(170, 23);
             this.txtNombre.TabIndex = 0;
@@ -62,7 +68,7 @@
             this.txtApellido.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtApellido.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtApellido.Location = new System.Drawing.Point(209, 122);
+            this.txtApellido.Location = new System.Drawing.Point(209, 130);
             this.txtApellido.Name = "txtApellido";
             this.txtApellido.Size = new System.Drawing.Size(170, 23);
             this.txtApellido.TabIndex = 1;
@@ -72,7 +78,7 @@
             this.txtDNI.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtDNI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDNI.Location = new System.Drawing.Point(209, 162);
+            this.txtDNI.Location = new System.Drawing.Point(209, 186);
             this.txtDNI.Name = "txtDNI";
             this.txtDNI.Size = new System.Drawing.Size(170, 23);
             this.txtDNI.TabIndex = 2;
@@ -82,7 +88,7 @@
             this.txtDireccion.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtDireccion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDireccion.Location = new System.Drawing.Point(209, 202);
+            this.txtDireccion.Location = new System.Drawing.Point(209, 242);
             this.txtDireccion.Name = "txtDireccion";
             this.txtDireccion.Size = new System.Drawing.Size(170, 23);
             this.txtDireccion.TabIndex = 3;
@@ -92,7 +98,7 @@
             this.txtCUIT.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtCUIT.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtCUIT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCUIT.Location = new System.Drawing.Point(209, 242);
+            this.txtCUIT.Location = new System.Drawing.Point(209, 298);
             this.txtCUIT.Name = "txtCUIT";
             this.txtCUIT.Size = new System.Drawing.Size(170, 23);
             this.txtCUIT.TabIndex = 4;
@@ -102,7 +108,7 @@
             this.txtNroCarnet.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtNroCarnet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtNroCarnet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNroCarnet.Location = new System.Drawing.Point(209, 282);
+            this.txtNroCarnet.Location = new System.Drawing.Point(209, 354);
             this.txtNroCarnet.Name = "txtNroCarnet";
             this.txtNroCarnet.Size = new System.Drawing.Size(170, 23);
             this.txtNroCarnet.TabIndex = 5;
@@ -112,7 +118,7 @@
             this.lblNombre.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(106, 83);
+            this.lblNombre.Location = new System.Drawing.Point(106, 76);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(62, 17);
             this.lblNombre.TabIndex = 6;
@@ -123,7 +129,7 @@
             this.lblApellido.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblApellido.AutoSize = true;
             this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblApellido.Location = new System.Drawing.Point(106, 123);
+            this.lblApellido.Location = new System.Drawing.Point(106, 132);
             this.lblApellido.Name = "lblApellido";
             this.lblApellido.Size = new System.Drawing.Size(62, 17);
             this.lblApellido.TabIndex = 7;
@@ -134,7 +140,7 @@
             this.lblDNI.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblDNI.AutoSize = true;
             this.lblDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDNI.Location = new System.Drawing.Point(133, 163);
+            this.lblDNI.Location = new System.Drawing.Point(133, 188);
             this.lblDNI.Name = "lblDNI";
             this.lblDNI.Size = new System.Drawing.Size(35, 17);
             this.lblDNI.TabIndex = 8;
@@ -145,7 +151,7 @@
             this.lblDireccion.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblDireccion.AutoSize = true;
             this.lblDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDireccion.Location = new System.Drawing.Point(97, 203);
+            this.lblDireccion.Location = new System.Drawing.Point(97, 244);
             this.lblDireccion.Name = "lblDireccion";
             this.lblDireccion.Size = new System.Drawing.Size(71, 17);
             this.lblDireccion.TabIndex = 9;
@@ -156,7 +162,7 @@
             this.lblCUIT.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblCUIT.AutoSize = true;
             this.lblCUIT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCUIT.Location = new System.Drawing.Point(125, 243);
+            this.lblCUIT.Location = new System.Drawing.Point(125, 300);
             this.lblCUIT.Name = "lblCUIT";
             this.lblCUIT.Size = new System.Drawing.Size(43, 17);
             this.lblCUIT.TabIndex = 10;
@@ -167,7 +173,7 @@
             this.lblNroCarnet.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblNroCarnet.AutoSize = true;
             this.lblNroCarnet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNroCarnet.Location = new System.Drawing.Point(42, 282);
+            this.lblNroCarnet.Location = new System.Drawing.Point(42, 356);
             this.lblNroCarnet.Name = "lblNroCarnet";
             this.lblNroCarnet.Size = new System.Drawing.Size(126, 17);
             this.lblNroCarnet.TabIndex = 11;
@@ -180,7 +186,7 @@
             this.btnAgregarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAgregarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAgregarCliente.ForeColor = System.Drawing.Color.White;
-            this.btnAgregarCliente.Location = new System.Drawing.Point(45, 320);
+            this.btnAgregarCliente.Location = new System.Drawing.Point(45, 414);
             this.btnAgregarCliente.Name = "btnAgregarCliente";
             this.btnAgregarCliente.Size = new System.Drawing.Size(106, 30);
             this.btnAgregarCliente.TabIndex = 12;
@@ -197,7 +203,7 @@
             this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancelar.ForeColor = System.Drawing.Color.White;
-            this.btnCancelar.Location = new System.Drawing.Point(273, 320);
+            this.btnCancelar.Location = new System.Drawing.Point(273, 414);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(106, 30);
             this.btnCancelar.TabIndex = 13;
@@ -211,6 +217,12 @@
             // 
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblValidNroCarnet);
+            this.panel1.Controls.Add(this.lblValidCUIT);
+            this.panel1.Controls.Add(this.lblValidDireccion);
+            this.panel1.Controls.Add(this.lblValidDni);
+            this.panel1.Controls.Add(this.lblValidApellido);
+            this.panel1.Controls.Add(this.lblValidNombre);
             this.panel1.Controls.Add(this.lblTitulo);
             this.panel1.Controls.Add(this.lblNombre);
             this.panel1.Controls.Add(this.btnCancelar);
@@ -226,26 +238,92 @@
             this.panel1.Controls.Add(this.lblDNI);
             this.panel1.Controls.Add(this.txtNroCarnet);
             this.panel1.Controls.Add(this.lblApellido);
-            this.panel1.Location = new System.Drawing.Point(82, 36);
+            this.panel1.Location = new System.Drawing.Point(82, 26);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(420, 380);
+            this.panel1.Size = new System.Drawing.Size(420, 460);
             this.panel1.TabIndex = 14;
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(146, 32);
+            this.lblTitulo.Location = new System.Drawing.Point(146, 22);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(142, 24);
             this.lblTitulo.TabIndex = 14;
             this.lblTitulo.Text = "Nuevo Cliente";
             // 
+            // lblValidNombre
+            // 
+            this.lblValidNombre.AutoSize = true;
+            this.lblValidNombre.ForeColor = System.Drawing.Color.Red;
+            this.lblValidNombre.Location = new System.Drawing.Point(209, 100);
+            this.lblValidNombre.Name = "lblValidNombre";
+            this.lblValidNombre.Size = new System.Drawing.Size(160, 13);
+            this.lblValidNombre.TabIndex = 15;
+            this.lblValidNombre.Text = "Debe contener al menos 3 letras";
+            this.lblValidNombre.Visible = false;
+            // 
+            // lblValidApellido
+            // 
+            this.lblValidApellido.AutoSize = true;
+            this.lblValidApellido.ForeColor = System.Drawing.Color.Red;
+            this.lblValidApellido.Location = new System.Drawing.Point(209, 156);
+            this.lblValidApellido.Name = "lblValidApellido";
+            this.lblValidApellido.Size = new System.Drawing.Size(160, 13);
+            this.lblValidApellido.TabIndex = 16;
+            this.lblValidApellido.Text = "Debe contener al menos 3 letras";
+            this.lblValidApellido.Visible = false;
+            // 
+            // lblValidDni
+            // 
+            this.lblValidDni.AutoSize = true;
+            this.lblValidDni.ForeColor = System.Drawing.Color.Red;
+            this.lblValidDni.Location = new System.Drawing.Point(209, 212);
+            this.lblValidDni.Name = "lblValidDni";
+            this.lblValidDni.Size = new System.Drawing.Size(162, 13);
+            this.lblValidDni.TabIndex = 17;
+            this.lblValidDni.Text = "El DNI debe contener 8 numeros";
+            this.lblValidDni.Visible = false;
+            // 
+            // lblValidDireccion
+            // 
+            this.lblValidDireccion.AutoSize = true;
+            this.lblValidDireccion.ForeColor = System.Drawing.Color.Red;
+            this.lblValidDireccion.Location = new System.Drawing.Point(209, 268);
+            this.lblValidDireccion.Name = "lblValidDireccion";
+            this.lblValidDireccion.Size = new System.Drawing.Size(131, 13);
+            this.lblValidDireccion.TabIndex = 18;
+            this.lblValidDireccion.Text = "Proporcione una dirección";
+            this.lblValidDireccion.Visible = false;
+            // 
+            // lblValidCUIT
+            // 
+            this.lblValidCUIT.AutoSize = true;
+            this.lblValidCUIT.ForeColor = System.Drawing.Color.Red;
+            this.lblValidCUIT.Location = new System.Drawing.Point(209, 324);
+            this.lblValidCUIT.Name = "lblValidCUIT";
+            this.lblValidCUIT.Size = new System.Drawing.Size(178, 13);
+            this.lblValidCUIT.TabIndex = 19;
+            this.lblValidCUIT.Text = "Este campo debe contener números";
+            this.lblValidCUIT.Visible = false;
+            // 
+            // lblValidNroCarnet
+            // 
+            this.lblValidNroCarnet.AutoSize = true;
+            this.lblValidNroCarnet.ForeColor = System.Drawing.Color.Red;
+            this.lblValidNroCarnet.Location = new System.Drawing.Point(209, 381);
+            this.lblValidNroCarnet.Name = "lblValidNroCarnet";
+            this.lblValidNroCarnet.Size = new System.Drawing.Size(178, 13);
+            this.lblValidNroCarnet.TabIndex = 20;
+            this.lblValidNroCarnet.Text = "Este campo debe contener números";
+            this.lblValidNroCarnet.Visible = false;
+            // 
             // FrmAltaClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 442);
+            this.ClientSize = new System.Drawing.Size(584, 512);
             this.Controls.Add(this.panel1);
             this.Name = "FrmAltaClientes";
             this.Text = "Nuevo Cliente";
@@ -273,5 +351,11 @@
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblValidNroCarnet;
+        private System.Windows.Forms.Label lblValidCUIT;
+        private System.Windows.Forms.Label lblValidDireccion;
+        private System.Windows.Forms.Label lblValidDni;
+        private System.Windows.Forms.Label lblValidApellido;
+        private System.Windows.Forms.Label lblValidNombre;
     }
 }
--- Vistas/FrmAltaClientes.cs
+++ Vistas/FrmAltaClientes.cs
@@ -30,28 +30,90 @@ namespace Vistas
 
         private void btnAgregarCliente_Click(object sender, EventArgs e)
         {
+            HideErrorLabels();
+            bool bErrorFound = false;
+            ValidateTextBoxs(ref bErrorFound);
 
-            string nombre = txtNombre.Text;
-            string apellido = txtApellido.Text;
-            string dni = txtDNI.Text;
-            string direccion = txtDireccion.Text;
-            string cuit = txtCUIT.Text;
-            string nroCarnet = txtNroCarnet.Text;
-
-            if (nombre != String.Empty && apellido != String.Empty && dni != String.Empty && direccion != String.Empty && cuit != String.Empty && nroCarnet != String.Empty)
+            if (!bErrorFound)
             {
+                string szNombre = txtNombre.Text;
+                string szApellido = txtApellido.Text;
+                string szDni = txtDNI.Text;
+                string szDireccion = txtDireccion.Text;
+                string szCuit = txtCUIT.Text;
+                string szNroCarnet = txtNroCarnet.Text;
+
                 DialogResult dialogResult = MessageBox.Show("¿Esta seguro de que desea guardar este registro?",
                 "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
                 if (dialogResult == DialogResult.Yes)
                 {
-                    Cliente oCliente = new Cliente(nombre, apellido, dni, direccion, cuit, nroCarnet);
-                    MessageBox.Show(oCliente.MostrarDatos(), "Cliente agregado");
+                    Cliente oCliente = new Cliente(szNombre, szApellido, szDni, szDireccion, szCuit, szNroCarnet);
+                    MessageBox.Show("Datos del cliente: " +
+                                     "\n\n DNI : " + oCliente.Cli_DNI +
+                                     "\n Apellido : " + oCliente.Cli_Apellido +
+                                     "\n Nombre : " + oCliente.Cli_Nombre +
+                                     "\n Dirección : " + oCliente.Cli_Direccion +
+                                     "\n CUIT OS: " + oCliente.Os_CUIT +
+                                     "\n Nro. de Carnet : " + oCliente.Cli_NroCarnet);
+                    ClearTextBoxs();
                 }
-            }
-            else
+
+            }                  
+        }
+
+        public void HideErrorLabels()
+        {
+            lblValidDni.Hide();
+            lblValidApellido.Hide();
+            lblValidNombre.Hide();
+            lblValidDireccion.Hide();
+            lblValidCUIT.Hide();
+            lblValidNroCarnet.Hide();
+        }
+
+        private void ClearTextBoxs()
+        {
+            txtDNI.Clear();
+            txtApellido.Clear();
+            txtNombre.Clear();
+            txtDireccion.Clear();
+            txtCUIT.Clear();
+            txtNroCarnet.Clear();
+        }
+
+        private void ValidateTextBoxs(ref bool bErrorFound)
+        {
+            if (txtDNI.Text.Length != 8)
             {
-                MessageBox.Show("Llene todos los campos.", "Faltan llenar campos", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }                       
+                lblValidDni.Show();
+                bErrorFound = true;
+            }
+            if (txtApellido.Text.Length < 3)
+            {
+                lblValidApellido.Show();
+                bErrorFound = true;
+            }
+            if (txtNombre.Text.Length < 3)
+            {
+                lblValidNombre.Show();
+                bErrorFound = true;
+            }
+            if (string.IsNullOrEmpty(txtDireccion.Text))
+            {
+                lblValidDireccion.Show();
+                bErrorFound = true;
+            }
+            if (!txtCUIT.Text.All(char.IsDigit) || string.IsNullOrEmpty(txtCUIT.Text))
+            {
+                lblValidCUIT.Show();
+                bErrorFound = true;
+            }
+            if (!txtNroCarnet.Text.All(char.IsDigit) || string.IsNullOrEmpty(txtNroCarnet.Text))
+            {
+                lblValidNroCarnet.Show();
+                bErrorFound = true;
+            }
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
